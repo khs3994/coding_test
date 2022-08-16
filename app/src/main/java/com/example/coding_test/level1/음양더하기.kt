ee6636b5151280ade77ed3c18a7ee79604dd301f
khs3994 <z01039948003@gmail.com>
2022-08-16 19:54:22 +0900

    음양더하기 08-16

---
+++ app/src/main/java/com/example/coding_test/level1/음양더하기.kt
@@ -0,0 +1,19 @@
+package com.example.coding_test.level1
+
+fun main() {
+    print(solution(intArrayOf(4,7,12), booleanArrayOf(true,false,true)))
+}
+
+fun solution(absolutes: IntArray, signs: BooleanArray): Int {
+    var answer: Int = 0
+
+    for (i in absolutes.indices) {
+        if (!signs[i]) {
+            answer -= absolutes[i]
+        } else {
+            answer += absolutes[i]
+        }
+    }
+
+    return answer
+}
