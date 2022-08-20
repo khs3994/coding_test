f860d9ceb7777c1b6945fd327497abf67efbaa02
khs3994 <z01039948003@gmail.com>
2022-08-20 23:01:11 +0900

    k 번째 수 08-20

---
+++ app/src/main/java/com/example/coding_test/level1/k번째 수.kt
@@ -0,0 +1,14 @@
+package com.example.coding_test.level1
+
+fun main() {
+    print(
+        solution(
+            intArrayOf(1, 5, 2, 6, 3, 7, 4),
+            arrayOf(intArrayOf(2, 5, 3), intArrayOf(4, 4, 1), intArrayOf(1, 7, 3))
+        )
+    )
+}
+
+fun solution(array: IntArray, commands: Array<IntArray>): IntArray =
+    commands.map { array.sliceArray(it[0] - 1 until it[1]).apply { sort() }[it[2] - 1] }
+        .toIntArray()
