1bf0771926a274955e04cfe45f7560316a22496f
김현승 <80810303+khs3994@users.noreply.github.com>
2022-09-15 21:01:41 +0900

    09-15 서울에서 김서방 찾기

---
+++ app/src/main/java/com/example/coding_test/level1/서울에서 김서방 찾기.kt
@@ -0,0 +1,11 @@
+package com.example.coding_test.level1
+
+fun main() {
+    print(solution(arrayOf("jane", "Kim")))
+}
+
+fun solution(seoul: Array<String>): String {
+    var answer = ""
+    seoul.withIndex().forEach { if (it.value == "Kim") answer = "김서방은 ${it.index}에 있다" }
+    return answer
+}
