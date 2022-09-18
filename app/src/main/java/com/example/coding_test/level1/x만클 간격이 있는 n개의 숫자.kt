d805db58f27d0fd32a08f7538cee520f31e62289
김현승 <80810303+khs3994@users.noreply.github.com>
2022-09-19 00:30:52 +0900

    09-18 x만큼 간격이 있는 n개의 숫자

--- app/src/main/java/com/example/coding_test/level1/x만클 간격이 있는 n개의 숫자.kt
+++ app/src/main/java/com/example/coding_test/level1/x만클 간격이 있는 n개의 숫자.kt
@@ -1,13 +1,8 @@
 package com.example.coding_test.level1
 
 fun main() {
-    print(solution2(2,5))
+    print(solution2(-2, 5))
 }
 
-fun solution2(x: Int, n: Int): LongArray {
-    var answer = longArrayOf()
-    for (i in 1..n) {
-        answer.plus((x * i).toLong())
-    }
-    return answer
-}
+fun solution2(x: Int, n: Int) = LongArray(n) { x.toLong() * (it + 1) }
+
