cddbe5c6adcc6df381021590772c2ea3c4cf59d7
khs3994 <z01039948003@gmail.com>
2022-09-22 20:47:00 +0900

    09-22 자릿수 더하기

---
+++ app/src/main/java/com/example/coding_test/level1/자릿수 더하기.kt
@@ -0,0 +1,15 @@
+package com.example.coding_test.level1
+
+fun main() {
+    print(solution4(123))
+}
+
+fun solution4(n: Int): Int {
+    var answer = 0
+    var temp = n
+    while (temp >= 1) {
+        answer += temp % 10
+        temp /= 10
+    }
+    return answer
+}
