e387cc1648a9957cb605799daf20574b54e5718b
khs3994 <z01039948003@gmail.com>
2022-09-22 20:42:11 +0900

    09-22 자연수 뒤집어 배열로 만들기

---
+++ app/src/main/java/com/example/coding_test/level1/자연수 뒤집에 배열로 만들기.kt
@@ -0,0 +1,16 @@
+package com.example.coding_test.level1
+
+fun main() {
+    print(solution4(12345))
+}
+
+fun solution4(n: Long): IntArray {
+    val result = mutableListOf<Int>()
+    var temp = n
+    while (temp >= 1) {
+        result.add((temp % 10).toInt())
+        temp /= 10
+        print(temp)
+    }
+    return result.toIntArray()
+}
