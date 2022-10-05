cc0ae5116c1e2dbcbded15cc17b4b65cf02eff53
khs3994 <z01039948003@gmail.com>
2022-10-05 23:33:31 +0900

    10-5 정수 제곱근 판별

---
+++ app/src/main/java/com/example/coding_test/level1/정수 제곱근 판별.kt
@@ -0,0 +1,17 @@
+package com.example.coding_test.level1
+
+import kotlin.math.sqrt
+
+fun main() {
+    print(solution(11L))
+}
+
+fun solution(n: Long): Long {
+    val num = sqrt(n.toDouble())
+    val temp = (num % 1) * 100
+    return if (temp.toInt() == 0) {
+        ((num + 1) * (num + 1)).toLong()
+    } else {
+        -1
+    }
+}
