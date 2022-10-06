8f68bbbe2e80e9c3b6bf1fd55cfe12c6762a1f4c
khs3994 <z01039948003@gmail.com>
2022-10-06 15:13:38 +0900

    10-6 핸드폰 번호 가리기

---
+++ app/src/main/java/com/example/coding_test/level1/핸드폰 번호 가리기.kt
@@ -0,0 +1,14 @@
+package com.example.coding_test.level1
+
+fun main() {
+    solution2("01039948003")
+}
+
+fun solution2(phone_number: String): String {
+    var answer = ""
+    for (i in 1..phone_number.length-4){
+        answer += "*"
+    }
+    answer += phone_number.substring(phone_number.length - 4,phone_number.length)
+    return answer
+}
