969e73d31c8305a457011ab91bb266d22eaf1f98
khs3994 <z01039948003@gmail.com>
2022-10-08 17:30:56 +0900

    10 - 8 가운데 글자 가져오기

---
+++ app/src/main/java/com/example/coding_test/level1/가운데 글자 가져오기.kt
@@ -0,0 +1,7 @@
+package com.example.coding_test.level1
+
+fun main() {
+    print(solution5("abcd"))
+}
+
+fun solution5(s: String): String = if(s.length % 2 != 0) s.substring(s.length/2, s.length/2+1) else s.substring(s.length/2-1 until (s.length/2+1))
