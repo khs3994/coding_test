52d2d59beaab86db8b51e2dc34ba0ccf3b323863
khs3994 <z01039948003@gmail.com>
2022-10-15 15:54:57 +0900

    10 - 15 콜라츠 추측

---
+++ app/src/main/java/com/example/coding_test/level1/콜라츠 추측.kt
@@ -0,0 +1,16 @@
+package com.example.coding_test.level1
+
+fun main() {
+    print(solution5(626331))
+}
+
+fun solution5(num: Int): Int {
+    var number: Long = num.toLong()
+    var cnt = 0
+    while (cnt <= 500) {
+        if (number.toInt() == 1) return cnt
+        number = if (number.toInt() % 2 == 0) number / 2 else number * 3 + 1
+        cnt++
+    }
+    return -1
+}
