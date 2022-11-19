2db303aaf705eb9d11adfe6346252b7b9d10bd17
khs3994 <z01039948003@gmail.com>
2022-11-19 10:08:44 +0900

    코테 특강 [로또 최고순위와 최저순위]

---
+++ app/src/main/java/com/example/coding_test/coding_test_class/로또 최고순위와 최저순위.kt
@@ -0,0 +1,15 @@
+package com.example.coding_test.coding_test_class
+
+fun main() {
+    print(solution(intArrayOf(44, 1, 0, 0, 31, 25), intArrayOf(31, 10, 45, 1, 6, 19)))
+}
+
+fun solution(lottos: IntArray, win_nums: IntArray): IntArray {
+    val array = arrayListOf<Int>()
+    val rank = intArrayOf(6, 6, 5, 4, 3, 2, 1)
+    val temp = lottos.filter { win_nums.contains(it) }.toList().size
+    val zero_cnt = lottos.filter { it == 0 }.toList().size
+    array.add(rank[temp + zero_cnt])
+    array.add(rank[temp])
+    return array.toIntArray()
+}
