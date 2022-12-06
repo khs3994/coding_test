b2d80f3fae8c30d02dd864226756b4f57a21610f
khs3994 <z01039948003@gmail.com>
2022-12-06 22:17:38 +0900

    [12-05] 백준(문자열) :: 알파벳 찾기

--- app/src/main/java/com/example/coding_test/baekjoon/문자열/알파벳 찾기.kt
+++ app/src/main/java/com/example/coding_test/baekjoon/문자열/알파벳 찾기.kt
@@ -3,6 +3,6 @@ package com.example.coding_test.baekjoon.문자열
 fun main() {
     val input = readLine()!!.toString()
     ('a'..'z').forEach {
-        print("${if (input.contains(it)) input.indexOf(it) else "-1"} ")
+        print("${input.indexOf(it)} ")
     }
 }
