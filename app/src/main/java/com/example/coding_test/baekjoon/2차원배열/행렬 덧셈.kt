1939fed356bab297b0edb6d2b0c590f3d5bd82ad
khs3994 <z01039948003@gmail.com>
2022-12-24 23:58:44 +0900

    [12-24] 백준(2차원배열) :: 행렬 덧셈

---
+++ app/src/main/java/com/example/coding_test/baekjoon/2차원배열/행렬 덧셈.kt
@@ -0,0 +1,19 @@
+package com.example.coding_test.baekjoon.`2차원배열`
+
+fun main() {
+    val size = readLine()!!.split(" ").map { it.toInt() }
+    val arr1 = arrayListOf<List<Int>>()
+    val arr2 = arrayListOf<List<Int>>()
+    repeat(size[1]) {
+        arr1.add(readLine()!!.split(" ").map { it.toInt() })
+    }
+    repeat(size[1]) {
+        arr2.add(readLine()!!.split(" ").map { it.toInt() })
+    }
+    for (i in 0 until size[0]) {
+        for (j in 0 until size[1]) {
+            print("${arr1[i][j] + arr2[i][j]} ")
+        }
+        println()
+    }
+}
